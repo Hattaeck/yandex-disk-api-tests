d8128d46ed15c7ea0a5137154c191e9fa79206f3
Aleksey <99037915+Hattaeck@users.noreply.github.com>
2026-04-09 13:31:22 +0300

    Added negative path, invalid token, PATH, trash and e.t.c

--- src/test/java/ru/yandex/disk/tests/DiskApiTest.java
+++ src/test/java/ru/yandex/disk/tests/DiskApiTest.java
@@ -77,8 +77,102 @@ public class DiskApiTest {
                 .statusCode(201);
     }
 
+    @Test
+    public void testCreateDuplicateFolderFails() {
+        String folderName = "DuplicateFolder_" + System.currentTimeMillis();
 
+        given()
+                .header("Authorization", "OAuth " + myToken)
+                .queryParam("path", folderName)
+                .when()
+                .put("https://cloud-api.yandex.net/v1/disk/resources")
+                .then()
+                .statusCode(201);
 
+        given()
+                .header("Authorization", "OAuth " + myToken)
+                .queryParam("path", folderName)
+                .when()
+                .put("https://cloud-api.yandex.net/v1/disk/resources")
+                .then()
+                .statusCode(409);
+    }
+
+    @Test
+    public void testGetDiskInfoWithInvalidToken() {
+        given()
+                .header("Authorization", "OAuth " + "invalid_token_123")
+                .when()
+                .get("https://cloud-api.yandex.net/v1/disk/")
+                .then()
+                .statusCode(401);
+    }
+
+    @Test
+    public void testDeleteNonExistentFolder() {
+        String nonExistentPath = "GhostFolder_" + System.currentTimeMillis();
+
+        given()
+                .header("Authorization", "OAuth " + myToken)
+                .queryParam("path", nonExistentPath)
+                .when()
+                .delete("https://cloud-api.yandex.net/v1/disk/resources")
+                .then()
+                .statusCode(404);
+    }
+
+    @Test
+    public void testUpdateFolderCustomProperties() {
+        String folderName = "PropertyTest_" + System.currentTimeMillis();
+
+        given()
+                .header("Authorization", "OAuth " + myToken)
+                .queryParam("path", folderName)
+                .when()
+                .put("https://cloud-api.yandex.net/v1/disk/resources")
+                .then()
+                .statusCode(201);
+
+        String body = "{\"custom_properties\": {\"test_key\": \"test_value\"}}";
+
+        given()
+                .header("Authorization", "OAuth " + myToken)
+                .header("Content-Type", "application/json")
+                .queryParam("path", folderName)
+                .body(body)
+                .when()
+                .patch("https://cloud-api.yandex.net/v1/disk/resources")
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    public void testGetTrashContents() {
+        given()
+                .header("Authorization", "OAuth " + myToken)
+                .when()
+                .get("https://cloud-api.yandex.net/v1/disk/trash/resources")
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    public void testPublishFolder() {
+        String folderName = "PublicFolder_" + System.currentTimeMillis();
+
+        given()
+                .header("Authorization", "OAuth " + myToken)
+                .queryParam("path", folderName)
+                .put("https://cloud-api.yandex.net/v1/disk/resources");
+
+        given()
+                .header("Authorization", "OAuth " + myToken)
+                .queryParam("path", folderName)
+                .when()
+                .put("https://cloud-api.yandex.net/v1/disk/resources/publish")
+                .then()
+                .statusCode(200);
+    }
 }
 
 
