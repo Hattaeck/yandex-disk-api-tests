263ba230023be492723a1448410322dd596aa218
Aleksey <99037915+Hattaeck@users.noreply.github.com>
2026-04-10 12:00:31 +0300

    Split Endpoints constants into main class, added more tests

---
+++ src/main/java/ru/yandex/disk/Endpoints.java
@@ -0,0 +1,11 @@
+package ru.yandex.disk;
+
+public class Endpoints {
+    public static final String DISK = "v1/disk/";
+    public static final String RESOURCES = "v1/disk/resources";
+    public static final String COPY = "v1/disk/resources/copy";
+    public static final String MOVE = "v1/disk/resources/move";
+    public static final String TRASH = "v1/disk/trash/resources";
+    public static final String PUBLISH = "v1/disk/resources/publish";
+    public static final String UNPUBLISH = "v1/disk/resources/unpublish";
+}
--- src/test/java/ru/yandex/disk/tests/DiskApiTest.java
+++ src/test/java/ru/yandex/disk/tests/DiskApiTest.java
@@ -1,180 +1,104 @@
 package ru.yandex.disk.tests;
 
-
-import static io.restassured.RestAssured.*;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import ru.yandex.disk.Endpoints;
+
+import static io.restassured.RestAssured.given;
 
 public class DiskApiTest {
 
-    private final String myToken = "FOR YANDEX TEAM OPEN READ ME";
+    private static RequestSpecification requestSpec;
+    private static final String MY_TOKEN = System.getenv("YANDEX_TOKEN");
+    @BeforeAll
+    public static void setup() {
+        requestSpec = new RequestSpecBuilder()
+                .setBaseUri("https://cloud-api.yandex.net/")
+                .addHeader("Authorization", "OAuth " + MY_TOKEN)
+                .setContentType(ContentType.JSON)
+                .log(LogDetail.ALL)
+                .build();
+    }
+
 
     @Test
+    @DisplayName("GET: Инфо о диске")
     public void testGetDiskInfo() {
-
-        given()
-                .header("Authorization", "OAuth " + myToken )
-                .when()
-                .get("https://cloud-api.yandex.net/v1/disk/")
-                .then()
-                .statusCode(200)
-                .log().all();
+        given().spec(requestSpec).when().get(Endpoints.DISK).then().statusCode(200);
     }
 
     @Test
+    @DisplayName("PUT: Создать папку")
     public void testCreateFolder() {
-        String folderName = "YandexTest_" + System.currentTimeMillis();
-
-        given()
-                .header("Authorization", "OAuth " + myToken)
-                .queryParam("path", folderName)
-                .when()
-                .put("https://cloud-api.yandex.net/v1/disk/resources")
-                .then()
-                .statusCode(201);
+        String path = "Folder_" + System.currentTimeMillis();
+        given().spec(requestSpec).queryParam("path", path).when().put(Endpoints.RESOURCES).then().statusCode(201);
     }
 
     @Test
-    public void testCreateAndDeleteFolder() {
-        String folderName = "FolderToDelete_" + System.currentTimeMillis();
-
-        given()
-                .header("Authorization", "OAuth " + myToken)
-                .queryParam("path", folderName)
-                .when()
-                .put("https://cloud-api.yandex.net/v1/disk/resources")
-                .then()
-                .statusCode(201);
-
-        given()
-                .header("Authorization", "OAuth " + myToken)
-                .queryParam("path", folderName)
-                .when()
-                .delete("https://cloud-api.yandex.net/v1/disk/resources")
-                .then()
-                .statusCode(204);
-    }
-
-    @Test
+    @DisplayName("POST: Копировать")
     public void testCopyFolder() {
-        String folderSource = "SourceFolder_" + System.currentTimeMillis();
-        String folderDestination = "DestFolder_" + System.currentTimeMillis();
-
-        given()
-                .header("Authorization", "OAuth " + myToken)
-                .queryParam("path", folderSource)
-                .when()
-                .put("https://cloud-api.yandex.net/v1/disk/resources")
-                .then()
-                .statusCode(201);
-
-        given()
-                .header("Authorization", "OAuth " + myToken)
-                .queryParam("from", folderSource)
-                .queryParam("path", folderDestination)
-                .when()
-                .post("https://cloud-api.yandex.net/v1/disk/resources/copy")
-                .then()
-                .statusCode(201);
+        String src = "Src_" + System.currentTimeMillis();
+        String dst = "Dst_" + System.currentTimeMillis();
+        given().spec(requestSpec).queryParam("path", src).put(Endpoints.RESOURCES);
+        given().spec(requestSpec).queryParam("from", src).queryParam("path", dst).when().post(Endpoints.COPY).then().statusCode(201);
     }
 
     @Test
-    public void testCreateDuplicateFolderFails() {
-        String folderName = "DuplicateFolder_" + System.currentTimeMillis();
-
-        given()
-                .header("Authorization", "OAuth " + myToken)
-                .queryParam("path", folderName)
-                .when()
-                .put("https://cloud-api.yandex.net/v1/disk/resources")
-                .then()
-                .statusCode(201);
-
-        given()
-                .header("Authorization", "OAuth " + myToken)
-                .queryParam("path", folderName)
-                .when()
-                .put("https://cloud-api.yandex.net/v1/disk/resources")
-                .then()
-                .statusCode(409);
+    @DisplayName("POST: Переместить (MOVE)")
+    public void testMoveFolder() {
+        String src = "MoveSrc_" + System.currentTimeMillis();
+        String dst = "MoveDst_" + System.currentTimeMillis();
+        given().spec(requestSpec).queryParam("path", src).put(Endpoints.RESOURCES);
+        given().spec(requestSpec).queryParam("from", src).queryParam("path", dst).when().post(Endpoints.MOVE).then().statusCode(201);
     }
 
     @Test
-    public void testGetDiskInfoWithInvalidToken() {
-        given()
-                .header("Authorization", "OAuth " + "invalid_token_123")
-                .when()
-                .get("https://cloud-api.yandex.net/v1/disk/")
-                .then()
-                .statusCode(401);
+    @DisplayName("DELETE: Удалить папку")
+    public void testDeleteResource() {
+        String path = "Del_" + System.currentTimeMillis();
+        given().spec(requestSpec).queryParam("path", path).put(Endpoints.RESOURCES);
+        given().spec(requestSpec).queryParam("path", path).when().delete(Endpoints.RESOURCES).then().statusCode(204);
     }
 
     @Test
-    public void testDeleteNonExistentFolder() {
-        String nonExistentPath = "GhostFolder_" + System.currentTimeMillis();
-
-        given()
-                .header("Authorization", "OAuth " + myToken)
-                .queryParam("path", nonExistentPath)
-                .when()
-                .delete("https://cloud-api.yandex.net/v1/disk/resources")
-                .then()
-                .statusCode(404);
+    @DisplayName("PUT: Опубликовать папку")
+    public void testPublishResource() {
+        String path = "Pub_" + System.currentTimeMillis();
+        given().spec(requestSpec).queryParam("path", path).put(Endpoints.RESOURCES);
+        given().spec(requestSpec).queryParam("path", path).when().put(Endpoints.PUBLISH).then().statusCode(200);
     }
 
     @Test
-    public void testUpdateFolderCustomProperties() {
-        String folderName = "PropertyTest_" + System.currentTimeMillis();
+    @DisplayName("DELETE: Отменить публикацию")
+    public void testUnpublishResource() {
+        String path = "Unpub_" + System.currentTimeMillis();
+
+        given().spec(requestSpec).queryParam("path", path).put(Endpoints.RESOURCES);
+        given().spec(requestSpec).queryParam("path", path).put(Endpoints.PUBLISH);
 
         given()
-                .header("Authorization", "OAuth " + myToken)
-                .queryParam("path", folderName)
+                .spec(requestSpec)
+                .queryParam("path", path)
                 .when()
-                .put("https://cloud-api.yandex.net/v1/disk/resources")
-                .then()
-                .statusCode(201);
-
-        String body = "{\"custom_properties\": {\"test_key\": \"test_value\"}}";
-
-        given()
-                .header("Authorization", "OAuth " + myToken)
-                .header("Content-Type", "application/json")
-                .queryParam("path", folderName)
-                .body(body)
-                .when()
-                .patch("https://cloud-api.yandex.net/v1/disk/resources")
+                .delete(Endpoints.UNPUBLISH)
                 .then()
                 .statusCode(200);
     }
 
     @Test
-    public void testGetTrashContents() {
-        given()
-                .header("Authorization", "OAuth " + myToken)
-                .when()
-                .get("https://cloud-api.yandex.net/v1/disk/trash/resources")
-                .then()
-                .statusCode(200);
+    @DisplayName("DELETE: Очистить корзину")
+    public void testClearTrash() {
+        given().spec(requestSpec).when().delete(Endpoints.TRASH).then().statusCode(204);
     }
 
     @Test
-    public void testPublishFolder() {
-        String folderName = "PublicFolder_" + System.currentTimeMillis();
-
-        given()
-                .header("Authorization", "OAuth " + myToken)
-                .queryParam("path", folderName)
-                .put("https://cloud-api.yandex.net/v1/disk/resources");
-
-        given()
-                .header("Authorization", "OAuth " + myToken)
-                .queryParam("path", folderName)
-                .when()
-                .put("https://cloud-api.yandex.net/v1/disk/resources/publish")
-                .then()
-                .statusCode(200);
+    @DisplayName("401: Плохой токен")
+    public void testInvalidToken() {
+        given().baseUri("https://cloud-api.yandex.net/").header("Authorization", "OAuth XXX").when().get(Endpoints.DISK).then().statusCode(401);
     }
 }
-
-
-
-
